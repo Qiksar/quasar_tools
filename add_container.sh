47555c78ba0f976c410336d95f28f3fae47b18f4
Qiksar Community <qiksar.apps@gmail.com>
2022-01-09 09:30:42 +0800

    Verbose list of container contents

--- add_container.sh
+++ add_container.sh
@@ -4,5 +4,7 @@ echo "*** Create .devcontainer folder if it does not exist..."
 
 echo
 echo "*** Pull the dev container configuration for Quasar"
+echo "*** Includes: QUASAR CLI, ICON GENIE, CORDOVA, YARN"
+echo
 curl -s --no-progress-meter https://raw.githubusercontent.com/chrisnurse/quasar_tools/main/devcontainer/devcontainer.json -o .devcontainer/devcontainer.json
 curl -s --no-progress-meter https://raw.githubusercontent.com/chrisnurse/quasar_tools/main/devcontainer/Dockerfile -o .devcontainer/Dockerfile.quasar
