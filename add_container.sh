b76b4606b5c941d196a4acb764305fba84a7951a
Qiksar Community <qiksar.apps@gmail.com>
2022-01-09 09:40:35 +0800

    Whoops, rename

--- add_container.sh
+++ add_container.sh
@@ -7,4 +7,4 @@ echo "*** Pull the dev container configuration for Quasar"
 echo "*** Includes: QUASAR CLI, ICON GENIE, CORDOVA, YARN"
 echo
 curl -s --no-progress-meter https://raw.githubusercontent.com/chrisnurse/quasar_tools/main/devcontainer/devcontainer.json -o .devcontainer/devcontainer.json
-curl -s --no-progress-meter https://raw.githubusercontent.com/chrisnurse/quasar_tools/main/devcontainer/Dockerfile -o .devcontainer/Dockerfile.quasar
+curl -s --no-progress-meter https://raw.githubusercontent.com/chrisnurse/quasar_tools/main/devcontainer/Dockerfile.quasar -o .devcontainer/Dockerfile.quasar
