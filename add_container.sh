0eec7f8538eff4a4fb596e018900c5b26815b254
Qiksar Community <qiksar.apps@gmail.com>
2022-01-09 09:26:01 +0800

    Tweak

--- add_container.sh
+++ add_container.sh
@@ -1,8 +1,8 @@
 echo
-echo "Create .devcontainer folder if it does not exist..."
+echo "*** Create .devcontainer folder if it does not exist..."
 [ ! -d "./.devcontainer" ] && mkdir .devcontainer 
 
 echo
-echo "Pull the dev container configuration for Quasar"
-curl https://raw.githubusercontent.com/chrisnurse/quasar_tools/main/devcontainer/devcontainer.json -o .devcontainer/devcontainer.json
-curl https://raw.githubusercontent.com/chrisnurse/quasar_tools/main/devcontainer/Dockerfile -o .devcontainer/Dockerfile.quasar
+echo "*** Pull the dev container configuration for Quasar"
+curl -s --no-progress-meter https://raw.githubusercontent.com/chrisnurse/quasar_tools/main/devcontainer/devcontainer.json -o .devcontainer/devcontainer.json
+curl -s --no-progress-meter https://raw.githubusercontent.com/chrisnurse/quasar_tools/main/devcontainer/Dockerfile -o .devcontainer/Dockerfile.quasar
